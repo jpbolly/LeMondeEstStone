21d43dfe750e4353cad93874d2094bf1f965a5ef
jpbolly <jp@bolly.eu>
2025-07-30 16:02:17 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Camera, Home, Library } from 'lucide-react-native';
+import { Camera, Chrome as Home, Library } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
